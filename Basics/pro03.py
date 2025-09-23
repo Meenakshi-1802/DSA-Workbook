7a2a6f7ca48bf2ddb2966c9ee4623b570fe43b9b
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-09-23 14:36:22 +0530

    Add files via upload

---
+++ Basics/pro03.py
@@ -0,0 +1,13 @@
+num = 10
+type(num)
+print(type(num))
+num1 = 2.5
+type(num1)
+print(type(num1))
+num2 = 5+6j
+type(num2)
+print(type(num2))
+num3 = True
+type(num3)
+print(type(num3))
+
---
+++ Basics/pro04.py
@@ -0,0 +1,8 @@
+list = [25,36,45,12]
+print(type(list))
+
+set = {25,36,45,15,12,25}
+print(type(set))
+
+tuple = (20,15,4,16,8,9)
+print(type(tuple))
