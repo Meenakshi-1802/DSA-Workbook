b519d0888e1fc0c8b871ab8d9d1242fb994ec6ad
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-09-23 21:24:32 +0530

    Add files via upload

---
+++ Basics/pro05.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("user is adult!can vote")
+else:
+    print("user can not vote")
---
+++ Basics/pro06.py
@@ -0,0 +1,9 @@
+light = (input("enter the colour:"))
+if light == "Red":
+    print("Please STOP!")
+elif light == "Yellow":
+    print("READY")
+elif light == "Green":
+    print("GO")
+else:
+    print("Invalid Light!")
